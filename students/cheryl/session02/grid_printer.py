08a4ca740287a9fea05e9346740e06803e4816e6
Cheryl Ohashi <cheryl.ohashi@gmail.com>
2015-10-17 20:01:22 +0000

    fizzbuzz soln

--- students/cheryl/session02/grid_printer.py
+++ students/cheryl/session02/grid_printer.py
@@ -8,3 +8,17 @@ def print_grid(n):
 print_grid(11)
 print_grid(5)
 print_grid(15)
+
+def print_grid_with_size(n,rows,cols):
+    size = (n-3)//2
+    cross_line = '+' + cols*(size*' -'+ ' +')  + '\n'
+    in_between = '|' + (cols-2)*' ' + '|' + (cols-2)*' ' + '|' +'\n'
+    print (rows*(cross_line + size*in_between) + cross_line)
+
+print_grid_with_size(15,2,2)
+print_grid_with_size(5,3,2)
+print_grid_with_size(5,3,4)
+
+
+
+
