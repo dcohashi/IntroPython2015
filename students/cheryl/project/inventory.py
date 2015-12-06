e70e814d69eef508502cb180f6b049f62481ee0d
Cheryl Ohashi <cheryl.ohashi@gmail.com>
2015-12-06 15:04:40 -0800

    fixed pep8

--- students/cheryl/project/inventory.py
+++ students/cheryl/project/inventory.py
@@ -5,15 +5,15 @@ import os.path
 from datetime import date
 import smtplib
 
+
 class FTInventory:
     '''
     Flight Test Equipment Inventory class
     '''
     def __init__(self, filename):
         self.fieldnames = ['FTID', 'Nomen', 'Asset Tag', 'Location',
-                      'Name', 'Date Checked Out', 'Expected Return']
+                           'Name', 'Date Checked Out', 'Expected Return']
         self.filename = filename
-
 
     def read(self):
         '''
@@ -33,69 +33,59 @@ class FTInventory:
         '''
         self.read()
         self.contents.append(new_record)
-        self.write()
 
     def write(self):
         '''
         Write out to the file
         '''
         with open(self.filename, "w") as f:
-            writer = csv.DictWriter(f, fieldnames = self.fieldnames)
+            writer = csv.DictWriter(f, fieldnames=self.fieldnames)
             writer.writeheader()
             for row in self.contents:
                 writer.writerow(row)
-    
 
     def add_equipment(self):
         '''
         Create a new record and add it to the file
-        Args:
-        fn - filename of inventory file
-        fieldnames - list of top row of csv file
-        Return:
-        None
         '''
-
         record = dict.fromkeys(self.fieldnames, None)
         record['FTID'] = input("FTID[]: ")
         record['Nomen'] = input("Nomenclature[]: ")
         record['Asset Tag'] = input("Asset Tag[]: ")
         record['Location'] = input("Location[]: ")
         self.append(record)
+        self.write()
 
     def list_equipment(self):
         '''
         Format and display contents
-        Args:
-        Return:
         '''
         self.read()
         print("   \tFTID\tAsset Tag\t\tNomen\tUser\tDate Checked Out")
         for index, row in enumerate(self.contents):
-            print("{} - \t{}\t\t{}\t\t{}\t\t{}\t\t{}".format(index+1, row['FTID'],
-                row['Asset Tag'], row['Nomen'], row['Name'], row['Date Checked Out']))
+            print("{} - \t{}\t\t{}\t\t{}\t\t{}\t\t{}".
+                  format(index+1, row['FTID'], row['Asset Tag'],
+                         row['Nomen'], row['Name'], row['Date Checked Out']))
         print("\n")
-
 
     def checkout_equipment(self):
         '''
-        Checkout equipement by selecting a record and adding more metadata to it
+        Checkout equipment by selecting a record
+        and adding more metadata to it
         '''
         # List the all the equipment, already checked out or not
         self.list_equipment()
 
         # Request user to select equipment
-        rownum = int(input("enter line number of equipment to check out: ")) -1
+        rownum = int(input
+                     ("enter line number of equipment to check out: ")) - 1
 
         # Prompt for additional metadata
         # Use today's date for checkout date
-
-        # update file
         self.contents[rownum]['Date Checked Out'] = str(date.today())
         self.contents[rownum]['Name'] = input("name: ")
         self.write()
 
-    
     def return_equipment(self):
         '''
         Remove the metadata to check in the equipment
@@ -104,7 +94,7 @@ class FTInventory:
         self.list_equipment()
 
         # Request user to select equipment
-        rownum = int(input("enter line number of equipment to return: ")) -1
+        rownum = int(input("enter line number of equipment to return: ")) - 1
 
         # Remove the meta data for that record
         self.contents[rownum]['Date Checked Out'] = None
@@ -113,11 +103,7 @@ class FTInventory:
 
     def del_equipment(self):
         '''
-        Delete a piece of inventory 
-        Args:
-            None
-        Return:
-         None
+        Delete a piece of inventory
         '''
         # List the all the equipment, already checked out or not
         self.list_equipment()
@@ -130,25 +116,35 @@ class FTInventory:
         self.write()
 
     def send_email(self):
+        '''
+        send email to admin
+        '''
         self.read()
-        message = "hi"
+        message = str()
+        message.join("   \tFTID\tAsset Tag\t\tNomen\tUser\tDate Checked Out")
+        for index, row in enumerate(self.contents):
+            message.join("{} - \t{}\t\t{}\t\t{}\t\t{}\t\t{}".
+                         format(index+1,
+                                row['FTID'], row['Asset Tag'],
+                                row['Nomen'],
+                                row['Name'], row['Date Checked Out']))
         server = smtplib.SMTP('localhost')
         server.sendmail(triageuser@dst.com, admin@dst.com, message)
         server.quit()
 
-if __name__  == '__main__':
-# open data file and read in contents
+if __name__ == '__main__':
+    # open data file and read in contents
     ft = FTInventory("inventory.csv")
 
     ans = ''
     while ans != 'q':
-        ans = input("s-send email\n" + 
+        ans = input("s-send email\n" +
                     "c-checkout equipment\n" +
                     "a-add equipment\n" +
                     "r-return equipment\n" +
                     "d-delete equipment\n" +
                     "l-list equipment\n" +
-                    "q-quit\n[q]:")
+                    "q-quit\t[q]:")
         if ans == 'a':
             print("\nAdd Equipment to Inventory")
             ft.add_equipment()
@@ -170,4 +166,3 @@ if __name__  == '__main__':
         else:
             ans = 'q'
             print("Exiting")
-
